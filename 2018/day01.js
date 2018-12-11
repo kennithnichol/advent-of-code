95a7ed719865845f2878eb8ff9ecfb4718680da2
Ken Nichol <1321022+kennithnichol@users.noreply.github.com>
2018-12-10 22:18:26 -0800

    Remove nested loop and increase performance 10x

---
+++ 2018/day01.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+const findCalibrationFrequency = input =>
+    input.reduce((acc, frequency) => acc + frequency, 0);
+
+const findFirstRepeatedFrequency = input => {
+    const frequencies = { 0: true };
+    const stack = [0];
+    const freqQueue = [];
+
+    while (!freqQueue.length) {
+        input.forEach(frequencyShift => {
+            let currentFrequency = stack.pop() + frequencyShift;
+            stack.push(currentFrequency);
+            if (frequencies[currentFrequency]) {
+                freqQueue.push(currentFrequency);
+            } else {
+                frequencies[currentFrequency] = true;
+            }
+        });
+    }
+    return freqQueue[0];
+};
+
+
+fs.readFile('./day01-input.txt', (err, data) => {
+    const input = data.toString('utf8').split(/\r\n|\n/).map(Number);
+
+    console.time('Q1');
+    console.log(`Calibration frequency ${findCalibrationFrequency(input)}`);
+    console.timeEnd('Q1');
+
+    console.time('Q2');
+    console.log(`First repeated frequency ${findFirstRepeatedFrequency(input)}`);
+    console.timeEnd('Q2');
+});
