ffe4945fbed2c0af7e340b1fb7022a7c1e83fb1b
Kennith Nichol <1321022+kennithnichol@users.noreply.github.com>
2019-01-02 16:53:29 -0800

    parase input data in to data structure

--- 2018/day-03/solve.js
+++ 2018/day-03/solve.js
@@ -1,13 +1,37 @@
+const part1 = (input = []) => {
+	input.forEach(line => {
+		const lineData = parseLine(line);
+	});
+}
+
+const part2 = (input = []) => {
+	return 'passed';
+}
+
+const parseLine = (line = "") => {
+	const materialRegExp = /#(\d+) @ (\d+),(\d+): (\d+)x(\d)+/;
+	const data = materialRegExp.exec(line);
+	return {
+		id: Number.parseInt(data[1]),
+		left: Number.parseInt(data[2]),
+		top: Number.parseInt(data[3]),
+		width: Number.parseInt(data[4]),
+		height: Number.parseInt(data[5])
+	};
+}
+
 const fs = require('fs');
 
-fs.readFile('./data.txt', (err, data) => {
-	const input = data.toString('utf8').split(/\r\n|\n/)
-
+fs.readFile('./input.txt', (err, data) => {
+	if (err) {
+		throw err;
+	}
+	const input = data.toString('utf8').split(/\r\n|\n/);
 	console.log('Solving 2018 Day 03');
-	console.time('Q1');
-
-    console.timeEnd('Q1');
-    console.time('Q1');
-
-	console.timeEnd('Q1');
+	console.time('Part1');
+	console.log(`part 1: ${part1(input)}`);
+	console.timeEnd('Part1');
+	console.time('Part2');
+	console.log(`part 2: ${part2(input)}`);
+	console.timeEnd('Part2');
 });
